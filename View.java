1ba6f8b5642404ec5949f48b13c21e6fb278e0d8
AndreManik <andre.ivan.manik@gmail.com>
2017-06-24 16:30:23 +0300

    Add files via upload

--- View.java
+++ View.java
@@ -94,5 +94,7 @@ public class View extends JFrame implements ActionListener {
 
     }
 
+    public boolean canUndo() {return false;}
+    public boolean canRedo() {return false;}
     public void selectedTabChanged(){}
 }
