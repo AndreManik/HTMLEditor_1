6d9e3e08a798f4bc36fb6a2f948944a42fdf8e42
AndreManik <andre.ivan.manik@gmail.com>
2017-06-25 00:18:30 +0300

    Add files via upload

---
+++ HTMLFileFilter.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+/**
+ * Created by Andre on 25.06.2017.
+ */
+public class HTMLFileFilter extends FileFilter {
+
+    @Override
+    public boolean accept(File f) {
+        if (f.isDirectory()){
+            return true;
+        } else {
+            String fileName = f.getName().toLowerCase();
+            return fileName.endsWith(".html") || fileName.endsWith(".htm");
+        }
+
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
